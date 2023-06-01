f0db5cdf12bc6c7bfb70d1b0ef437cb1a25579a4
dharanchilukuri <54701801+dharanchilukuri@users.noreply.github.com>
2023-06-01 22:01:47 +0530

    Update index.js
    Updated icons

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 
 const main = document.querySelector('main');
-let string=['<i class="fa-brands fa-html5 fa-2xl" style="color: #c6602a;"></i>','<i class="fa-brands fa-html5 fa-2xl" style="color: #c6602a;"></i>', '<i class="fa-brands fa-html5 fa-2xl" style="color: #c6602a;"></i>', '<i class="fa-brands fa-html5 fa-2xl" style="color: #c6602a;"></i>'];
+let string=['<i class="fa-brands fa-html5 fa-2xl" style="color:#ff0000;"></i>','<i class="fa-brands fa-css3-alt fa-2xl" style="color: #1106ef;"></i>','<i class="fa-brands fa-js fa-2xl" style="color: #00602a;"></i>','<i class="fa-brands fa-bootstrap fa-2xl style:#<i class="fa-brands fa-bootstrap fa-2xl" style="color: #ffef00;"</i>'];
 
 for (let i = 0; i < string.length; i++) {
   setTimeout(() => {
     main.innerHTML +=`<dfn>${string[i]}</dfn>`;
     console.log(main);
-  }, i * 1000 );
+  }, i * 1500 );
 }
 
